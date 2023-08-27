ed7857059bf4d72344dece97d8faf4be2251e75f
Silva Mauricio Nicolás <113395913+SilvaMauricioN@users.noreply.github.com>
2023-08-27 20:40:54 -0300

    add validacion string

--- src/main/java/utn/frbb/tup/LaboratorioIII/controller/Validator.java
+++ src/main/java/utn/frbb/tup/LaboratorioIII/controller/Validator.java
@@ -4,15 +4,22 @@ import java.lang.reflect.Field;
 
 public class Validator {
     public static void ValidarCampos(Object dto) throws IllegalArgumentException, IllegalAccessException {
-        //obtengo los atributos de la instancia dto
-        Field[] campos = dto.getClass().getDeclaredFields();
+        if (dto instanceof String) {
+            String strValue = (String) dto;
+            if (strValue.trim().isEmpty()) {
+                throw new IllegalArgumentException("El campo no puede estar vacío");
+            }
+        }else {
+            //obtengo los atributos de la instancia dto
+            Field[] campos = dto.getClass().getDeclaredFields();
 
-        for(Field campo:campos){
-            campo.setAccessible(true);
-            Object valor = campo.get(dto);
-            //si algun valor de los atributos de la intancia esta vacio, arroja una excepcion
-            if(valor == null || (valor instanceof String && ((String) valor).trim().isEmpty())){
-                throw  new IllegalArgumentException("Falta el campo: " + campo.getName());
+            for (Field campo : campos) {
+                campo.setAccessible(true);
+                Object valor = campo.get(dto);
+                //si algun valor de los atributos de la intancia esta vacio, arroja una excepcion
+                if (valor == null || (valor instanceof String && ((String) valor).trim().isEmpty())) {
+                    throw new IllegalArgumentException("Falta el campo: " + campo.getName());
+                }
             }
         }
     }
